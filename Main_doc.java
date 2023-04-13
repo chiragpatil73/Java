f063c37f6a08e8152103ff11e86f05043d70c927
Chirag Patil <124192305+chiragpatil73@users.noreply.github.com>
2023-04-13 20:15:05 +0530

    Add files via upload

---
+++ Main_doc.java
@@ -0,0 +1,73 @@
+package Doctor_database;
+class Datatype
+{
+	String name[]= {"DR.Rufash Nayak","DR.Chirag Patil","DR.Iftekhar Ahmad","DR.Himanshu Kumar","DR.Purna Reddy","","","","","",""};
+	String   age[]= {"23","25","27","29","30","","","","","",""};
+	String gender[]= {"male","male","male","male","male","","","","","","","","","",""};
+	String spe[]= {"Dermatologists","Ophthalmologists","Endocrinologists","Cardiologists","Gastroenterologists","","","","","",""};
+	String exp[]= {"2 years","3 years","5 years","1 years","2 years","","","","",""};
+	String hosName[]= {"Apollo Hospitals","Apollo Hospitals","Apollo Hospitals","Apollo Hospitals","Apollo Hospitals","","","",""};
+	String solcase[]= {"50","40","30","80","100","","","","","","",""};
+	String salary[]= {"122000.56","23232.89","741235.12","85236.23","95175.32","","","","","","",""};
+}
+class Methods extends Datatype
+{
+	public void dr_rufash()
+	{	System.out.println("Hospital Name:"+hosName[0]);
+		System.out.println("Doctor Name:"+name[0]);
+		System.out.println("Doctor age:"+age[0]);
+		System.out.println("Doctor Gender:"+gender[0]);
+		System.out.println("Doctor Specitalization:"+spe[0]);
+		System.out.println("Doctor Solved Case:"+solcase[0]);
+		System.out.println("Doctor Salary:"+salary[0]);
+		System.out.println();
+	}
+	public void dr_chirag()
+	{	System.out.println("Hospital Name:"+hosName[1]);
+		System.out.println("Doctor Name:"+name[1]);
+		System.out.println("Doctor age:"+age[1]);
+		System.out.println("Doctor Gender:"+gender[1]);
+		System.out.println("Doctor Specitalization:"+spe[1]);
+		System.out.println("Doctor Solved Case:"+solcase[1]);
+		System.out.println("Doctor Salary:"+salary[1]);
+		System.out.println();
+	}
+	public void dr_iftekhar()
+	{	System.out.println("Hospital Name:"+hosName[2]);
+		System.out.println("Doctor Name:"+name[2]);
+		System.out.println("Doctor age:"+age[2]);
+		System.out.println("Doctor Gender:"+gender[2]);
+		System.out.println("Doctor Specitalization:"+spe[2]);
+		System.out.println("Doctor Solved Case:"+solcase[2]);
+		System.out.println("Doctor Salary:"+salary[2]);
+		System.out.println();
+	}
+	public void dr_himanshu()
+	{	System.out.println("Hospital Name:"+hosName[3]);
+		System.out.println("Doctor Name:"+name[3]);
+		System.out.println("Doctor age:"+age[3]);
+		System.out.println("Doctor Gender:"+gender[3]);
+		System.out.println("Doctor Specitalization:"+spe[3]);
+		System.out.println("Doctor Solved Case:"+solcase[3]);
+		System.out.println("Doctor Salary:"+salary[3]);
+		System.out.println();
+	}
+	public void dr_purna()
+	{	System.out.println("Hospital Name:"+hosName[4]);
+		System.out.println("Doctor Name:"+name[4]);
+		System.out.println("Doctor age:"+age[4]);
+		System.out.println("Doctor Gender:"+gender[4]);
+		System.out.println("Doctor Specitalization:"+spe[4]);
+		System.out.println("Doctor Solved Case:"+solcase[4]);
+		System.out.println("Doctor Salary:"+salary[4]);
+		System.out.println();
+	}
+}
+public class Main_doc {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
